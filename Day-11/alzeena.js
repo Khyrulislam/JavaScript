c717509a0291e81820c023fc3b2b36f73ab7fc47
Your Name <you@example.com>
2022-06-05 01:40:12 +0600

    Day-11

---
+++ Day-11/alzeena.js
@@ -0,0 +1,37 @@
+
+var orderCode = parseInt(prompt("Enter You Order Code : "));
+
+
+var OrderCodeNumber1 = 46768;
+var OrderCodeNumber2 = 46793;
+var OrderCodeNumber3 = 47678;
+var OrderCodeNumber4 = 47711;
+var OrderCodeNumber5 = 47716;
+
+if (OrderCodeNumber1 == orderCode){
+    document.write("Order Id = " + orderCode  + "<br/>" + "Name : Mahadi Bin" + "<br/>" + "Phone : 01890682965" + "<br/>" + "Address : Noakhali sonainuri" + "<br/>" + "Due : 1860" + "<br/>" + "Coruier : Pathao" );
+    
+}
+
+else if  (OrderCodeNumber2 == orderCode){
+    document.write("Order Id = " + orderCode  + "<br/>" + "Name : Limon Hasan" + "<br/>" + "Phone : 01890682965" + "<br/>" + "|Address : Dagerchala,Hariken Road, Gazipur" + "<br/>" + "Due : 1860" + "<br/>" + "Coruier : Pathao" );
+    
+}
+
+else if  (OrderCodeNumber3 == orderCode){
+    document.write("Order Id = " + orderCode  + "<br/>" + "Name : Md.golam Mabud" + "<br/>" + "Phone : 01711536999" + "<br/>" + "|Address : Level #14,Flat # 14D,Dhaka-1000" + "<br/>" + "Due : 1020" + "<br/>" + "Coruier : Pathao" );
+    
+} 
+
+else if  (OrderCodeNumber4 == orderCode){
+    document.write("Order Id = " + orderCode  + "<br/>" + "Name : Sujon khan" + "<br/>" + "Phone : 01300379027" + "<br/>" + "|Address : Matuail, mridhabari,jatrabari dhaka 1362 " + "<br/>" + "Due : 1930" + "<br/>" + "Coruier : Pathao" );
+    
+} 
+
+else if  (OrderCodeNumber5 == orderCode){
+    document.write("Order Id = " + orderCode  + "<br/>" + "Name : Mahadi Bin" + "<br/>" + "Phone : 01890682965" + "<br/>" + "|Address : Noakhali sonainuri" + "<br/>" + "Due : 1860" + "<br/>" + "Coruier : Pathao" );
+    
+} 
+
+else (orderCode)
+document.write("........");
