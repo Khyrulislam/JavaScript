3902021a6ba7ab4c073dab18f46dc6240f2893cd
Your Name <you@example.com>
2022-06-06 01:34:14 +0600

    Day-12

---
+++ Day-12/index.js
@@ -0,0 +1,38 @@
+
+// function section 
+
+function plus (x,y){
+    var plus1 = x+y;
+    document.write("Sub = " + plus1 + "<br>");
+}
+
+function mines (x,y){
+    var mines1 = x-y;
+    document.write("pus = " + mines1 + "<br>");
+}
+
+function ghun (x,y){
+    var ghun1 = x*y;
+    document.write("Intu = " + ghun1 + "<br>");
+}
+
+function vag (x,y){
+    var vag1 = x/y;
+    document.write("Divided = " + vag1 + "<br>");
+}
+
+function vagSaesh (x,y){
+    var vagSaesh1 = x%y;
+    document.write("Modulus = " + vagSaesh1 + "<br>");
+}
+
+// funciton plus
+plus(10,12);
+// funciton mines
+mines(14,12);
+// funciton ghun
+ghun(15,15);
+// function vag
+vag(14,153);
+// function vagSaes
+vagSaesh(14,153);
