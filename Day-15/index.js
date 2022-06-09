6890e6ba2e8ce80e82dbc8f1a805e35b1c7e3c38
Your Name <you@example.com>
2022-06-10 01:26:43 +0600

    Day-15

---
+++ Day-15/index.js
@@ -0,0 +1,16 @@
+
+
+
+var number = [12,15,21,45,4,5,6,78,];
+
+number.sort(function (a,b){
+    return a+b ;
+});
+
+document.write(number);
+document.write("<br>")
+var latter = ['d','e','a','w','i'];
+
+var letter = latter.sort();
+
+document.write(letter);
