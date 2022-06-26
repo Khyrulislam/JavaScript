1571a6af4c4cca427d8ce660bad0ef2dcf965f90
Khyrul_Islam <100412419+Khyrulislam@users.noreply.github.com>
2022-06-27 02:16:34 +0600

    Add files via upload

---
+++ Day-24/index.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+var hadding1 = document.createElement("h2");
+var text = document.createTextNode("This is me");
+hadding1.appendChild(text);
+
+var divClass = document.querySelector(".div-new");
+divClass.appendChild(hadding1);
+
+
+
